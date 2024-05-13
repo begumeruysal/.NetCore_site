fc49bf3a4c919c41210c40ec5a4fe630b7508deb
begumeruysal <begumeruysal@gmail.com>
2024-05-14 02:38:10 +0300

    MVC Deneme

---
+++ BegümIşıkEruysalTechCareerMVCSitem/TechCareerMVCSitem/TechCareerMVCSitem/Controllers/AraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TechCareerMVCSitem.Data;
+using TechCareerMVCSitem.Models;
+
+public class AraController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public AraController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index(string q)
+    {
+        if (string.IsNullOrEmpty(q))
+        {
+            ViewBag.SearchQuery = "Arama yapılmadı.";
+            return View();
+        }
+
+        var products = _context.YeniUrun.Where(p => p.urunAdi.Contains(q)).ToList();
+
+        if (products.Any())
+        {
+            return View("Index", "YeniUrun");
+        }
+        else
+        {
+            ViewBag.SearchQuery = "Ürün bulunamadı.";
+            return View();
+        }
+    }
+
+}
---
+++ BegümIşıkEruysalTechCareerMVCSitem/TechCareerMVCSitem/TechCareerMVCSitem/Models/Ara.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechCareerMVCSitem.Models
+{
+    public class Ara
+    {
+        [Required]
+        public string urunAdi { get; set; }
+    }
+}
